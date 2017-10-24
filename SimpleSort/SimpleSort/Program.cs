8095803e95bcb3dda8fcae56bb5290c9d33f31ba
andycnzh <andycnzh@outlook.com>
2017-10-24 16:17:47 +0800

    Add simple sort implementation

---
+++ SimpleSort/SimpleSort/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SimpleSort simpleSort = new SimpleSort();
+            int[] unsortedArray = new int[] { 7, 8, 5, 2, 4, 6, 3 };
+            foreach (var item in unsortedArray)
+            {
+                Console.Write(item + " ");
+            }
+
+            Console.WriteLine();
+
+            int[] sortedArray = simpleSort.Sort(unsortedArray);
+            foreach (var item in sortedArray)
+            {
+                Console.Write(item + " ");
+            }
+
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SimpleSort/SimpleSort/SimpleSort.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleSort
+{
+    class SimpleSort
+    {
+        public int[] Sort(int[] unsortedArray)
+        {
+            int temp;
+            int[] sortedArray = new int[unsortedArray.Length];
+
+            for (int i = 0; i <= sortedArray.Length - 1; i++)
+            {
+                temp = 0;
+
+                for (int j = 0; j < unsortedArray.Length - 1; j++)
+                {
+                    if (unsortedArray[j] > unsortedArray[j + 1])
+                    {
+                        if (unsortedArray[temp] > unsortedArray[j + 1])
+                        {
+                            temp = j + 1;
+                        }
+                    }
+                    else
+                    {
+                        if (unsortedArray[temp] > unsortedArray[j])
+                        {
+                            temp = j;
+                        }
+                    }
+                }
+
+                sortedArray[i] = unsortedArray[temp];
+
+                unsortedArray[temp] = int.MaxValue;
+            }
+
+            return sortedArray;
+        }
+    }
+}
